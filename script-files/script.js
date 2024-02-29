602c34fffd32b0d9621ed9eb67b18f05d82c374a
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-29 23:41:46 +0600

    displayed category wise video & initially all videos

--- script-files/script.js
+++ script-files/script.js
@@ -14,35 +14,40 @@ const handleCategory = (categories) => {
 
   categories.forEach((category) => {
     const newBtn = document.createElement("button");
-    newBtn.classList = `bg-[#25252533] hover:bg-gray-200 hover:text-red-500 px-4 py-1 rounded text-black font-medium`;
+    newBtn.classList = `category-btn bg-[#25252533] hover:bg-gray-200 hover:text-red-500 px-4 py-1 rounded text-black font-medium`;
     newBtn.innerHTML = `${category.category}`;
     categoryContainer.appendChild(newBtn);
+    
+    // add event listener in the category button
+    newBtn.addEventListener('click', () => loadVideo(category.category_id));
   });
 };
 
 // load videos info from api
-const loadVideo = async () => {
-  const videoUrl = `https://openapi.programming-hero.com/api/videos/category/1000`;
+const loadVideo = async (categoryId) => {
+  const videoUrl = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`;
   const res = await fetch(videoUrl);
   const data = await res.json();
   handleVideos(data.data);
 };
-loadVideo();
 
 // handle videos
 const handleVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
+  
+  //clear before display new video
+  videoContainer.innerHTML= ''; 
+
+
 
   videos.map((video) => {
     const { title, thumbnail, authors, others } = video;
-    console.log(video);
-
     // dynamic video card
     const newCard = document.createElement("div");
-    newCard.classList = `card max-w-80`;
+    newCard.classList = `w-[310px] mx-auto`;
     newCard.innerHTML = `
-      <figure class="max-h-48 relative" >
-        <img src=${thumbnail} alt=${title} class="h-full w-full"/>
+      <figure class="h-[160px] relative" >
+        <img src=${thumbnail} alt=${title} class="h-full w-full rounded-xl hover:rounded-none hover:opacity-60"/>
         <div class="absolute right-4 bottom-4 bg-black bg-opacity-80 text-gray-300 text-xs py-1 px-2 rounded">${others.posted_date}</div>
       </figure>
 
@@ -63,4 +68,10 @@ const handleVideos = (videos) => {
   });
 };
 
+
+
+
 loadCategory();
+loadVideo('1000');
+
+
